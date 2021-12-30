d04090fe00582d1da51ad1e00b882ef36e797eb6
Mario Ramb <marioramb@gmail.com>
2021-12-30 11:07:50 -0300

    add identity

---
+++ WebApplicationPrueba/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplicationPrueba.Data;
+
+[assembly: HostingStartup(typeof(WebApplicationPrueba.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplicationPrueba.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
